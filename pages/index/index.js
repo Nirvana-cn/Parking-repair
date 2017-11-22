0f839fc7ec576a12830b409024fb953071c79496
Alliance <574014131@qq.com>
2017-11-22 20:21:47 +0800

    --

--- pages/index/index.js
+++ pages/index/index.js
@@ -11,23 +11,34 @@ Page({
       id: 10002,
       location: '浙江警官学院'
     }],
-    loadData:''
+    loadData: ''
   },
   onLoad: function () {
-
-  },
-  checkboxChange(res){
-    console.log(res.detail.value)
-    this.data.loadData=res.detail.value
-    console.log(this.data.loadData)
-  },
-  repair(){
-    let str=JSON.stringify(this.data.loadData)
     wx.request({
       url: 'http://120.25.200.217:8080/IparkingWeb/Repair.action',
-      data:{
-        sid:str,
-        reason:'111'
+      data: {
+        
+      },
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function (res) {
+        console.log(res.data)
+      }
+    })
+  },
+  checkboxChange(res) {
+    console.log(res.detail.value)
+    this.data.loadData = res.detail.value
+    console.log(this.data.loadData)
+  },
+  repair() {
+    let str = JSON.stringify(this.data.loadData)
+    wx.request({
+      url: 'http://120.25.200.217:8080/IparkingWeb/Repair.action',
+      data: {
+        sid: str,
+        reason: '111'
       },
       header: {
         'content-type': 'application/json' // 默认值
