a26e9291f1bc2c30750b83b8980311a9cb7ef3e9
Alliance <574014131@qq.com>
2017-11-22 20:19:07 +0800

    --

---
+++ pages/index/index.js
@@ -0,0 +1,40 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    items: [{
+      id: 10001,
+      location: '浙江理工大学'
+    }, {
+      id: 10002,
+      location: '浙江警官学院'
+    }],
+    loadData:''
+  },
+  onLoad: function () {
+
+  },
+  checkboxChange(res){
+    console.log(res.detail.value)
+    this.data.loadData=res.detail.value
+    console.log(this.data.loadData)
+  },
+  repair(){
+    let str=JSON.stringify(this.data.loadData)
+    wx.request({
+      url: 'http://120.25.200.217:8080/IparkingWeb/Repair.action',
+      data:{
+        sid:str,
+        reason:'111'
+      },
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function (res) {
+        console.log(res.data)
+      }
+    })
+  }
+})
